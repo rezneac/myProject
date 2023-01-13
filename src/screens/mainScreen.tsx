8d1715307ba481a1e357eb843af93aca7948ca1d
rezneac <rezneacanatolie@gmail.com>
2023-01-13 23:52:38 +0000

    Creating  checkBoxs and functional to them.

--- src/screens/mainScreen.tsx
+++ src/screens/mainScreen.tsx
@@ -1,16 +1,46 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import Checkbox from "expo-checkbox";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const mainScreen = () => {
-    return(
-        <View>
-            <Text>Hello World!</Text>
-        </View>
-    )
+  const [array, setArray] = useState([
+    { name: "Test", checked: true },
+    { name: "Test2", checked: true },
+    { name: "React", checked: true },
+    { name: "Native", checked: true },
+    { name: "Hello", checked: true },
+    { name: "Cool", checked: true },
+    { name: "tech", checked: true },
+  ]);
+
+  const newState = (index:number) => {
+    const newArrayState = [...array];
+    //changes from true to false checked property
+    newArrayState[index].checked = !newArrayState[index].checked;
+    setArray(newArrayState);
+}
+
+  return (
+    <View>
+      <View>
+        <FlatList
+          data={array}
+          horizontal={false}
+          renderItem={({ item, index }) => (
+            <View style={{ justifyContent: "center" }}>
+              <Text>{item.name}</Text>
+              <Checkbox
+                value={item.checked}
+                onValueChange={() => newState(index)}
+              />
+            </View>
+          )}
+        />
+      </View>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-    
-});
+const styles = StyleSheet.create({});
 
 export default mainScreen;
